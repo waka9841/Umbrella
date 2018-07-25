d182488b20c8998cc9921d2f8728807e3b90f7c5
Daisuke Kawasaki <daisuke.kawasaki@gmo.media>
2018-07-25 19:19:35 +0900

    location1~3各1000本ずつ傘を持たせた

--- app/controllers/input_controller.rb
+++ app/controllers/input_controller.rb
@@ -3,7 +3,7 @@ class InputController < ApplicationController
   	@location1 = Location.create(name: '渋谷109', longitude: '109_longitude', latitude: '109_latitude')
   	@location2 = Location.create(name: '渋谷駅', longitude: '渋谷駅_longitude', latitude: '渋谷駅_latitude')
   	@location3 = Location.create(name: 'セルリアンタワー', longitude: 'セルリアン_longitude', latitude: 'セルリアン_latitude')
-  	for num in 1..10 do
+  	for num in 1..1000 do
   		@location1.kasas.create(state: 1)
   		@location2.kasas.create(state: 1)
   		@location3.kasas.create(state: 1)  		
