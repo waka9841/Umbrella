aa369dc54486010f938d4ada193234550bfa00e7
Daisuke Kawasaki <daisuke.kawasaki@gmo.media>
2018-07-25 15:21:37 +0900

    デザインを当てている途中

--- app/controllers/input_controller.rb
+++ app/controllers/input_controller.rb
@@ -15,7 +15,7 @@ class InputController < ApplicationController
   end
 
   def show
-  	@location_id = params[:post][:location]
+  	@location_id = params[:location]
   	@umbrellaCount = Kasa.where(location_id: @location_id, state: 1).count
   end
 
